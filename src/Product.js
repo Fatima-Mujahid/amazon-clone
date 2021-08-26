5a3bf0071eb5f12a010e2a4a2b29fd80271f2c80
Fatima-Mujahid <65939673+Fatima-Mujahid@users.noreply.github.com>
2021-08-26 13:31:58 +0500

    feat(Product): create product with info

---
+++ src/Product.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Product.css';
+
+function Product() {
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p className="product__title">
+          The Lean Startup: How Constant Innovation Creates Radically Successful
+          Businesses Paperback
+        </p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>11.96</strong>
+        </p>
+        <div className="product__rating">
+          <p>🌟</p>
+        </div>
+      </div>
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+        alt="Product"
+        className="product__image"
+      />
+      <button className="product__button">Add to Basket</button>
+    </div>
+  );
+}
+
+export default Product;
