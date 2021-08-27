92307df4f12098608c604cc4f46c5e4364b827ab
Fatima-Mujahid <65939673+Fatima-Mujahid@users.noreply.github.com>
2021-08-27 17:16:44 +0500

    feat(Checkout): add checkout component

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import './Checkout.css';
+import Subtotal from './Subtotal';
 
 function Checkout() {
-  return <div>Checkout</div>;
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+          alt="Checkout Ad"
+          className="checkout__ad"
+        />
+        <h2 className="checkout__title">Your Shopping Basket</h2>
+        {/* BasketItem */}
+        {/* BasketItem */}
+        {/* BasketItem */}
+        {/* BasketItem */}
+      </div>
+      <div className="checkout__right">
+        <Subtotal />
+      </div>
+    </div>
+  );
 }
 
 export default Checkout;
